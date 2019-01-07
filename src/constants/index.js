6ce6af7b7dfc18c8d518b44abdbdbc847a4a6045
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-07 11:59:30 -0500

    updarted base_url for deployment

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'http://localhost:3001';
-export const API_WS_ROOT = 'ws://localhost:3001/cable';
+export const API_ROOT = 'https://oinkedin-backend.herokuapp.com';
+export const API_WS_ROOT = 'ws://oinkedin-backend.herokuapp.com/cable';
 
 export const HEADERS = {
   'Content-Type': 'application/json',
